d4e18882cefcd5c30578ab39cee85a61fea94cf7
desmond.chong <desmond.chong@beans.com.my>
2025-11-03 21:07:47 +0800

    Adding functionality for user to get a list of movie using searching keywords

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
 const inputEl = document.querySelector("input")
-document.querySelector("form").addEventListener('submit', (e) => {
+const imdbIDList = []
+let movieList = []
+const watchList = []
+document.querySelector("form").addEventListener('submit', async (e) => {
     e.preventDefault()
-    fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=4f70a2f8&t=${inputEl.value}`)
-        .then(res => res.json())
-        .then(data => console.log(data))
+    const responseMovieId = await fetch(`https://www.omdbapi.com/?apikey=4f70a2f8&s=${inputEl.value}`)
+    const movieIdData = await responseMovieId.json()
+    const movieIDList = movieIdData.Search.map(movie => movie.imdbID)
+    movieList= await Promise.all(
+        movieIDList.map(async (movieID) => {
+            const response = await fetch(`https://www.omdbapi.com/?apikey=4f70a2f8&i=${movieID}`)
+            return response.json()
+        })
+    )
+    document.querySelector("main").innerHTML = movieList.map(
+        movie =>
+        `
+        <div class="container">
+            <section class="movie-section">
+                <img src=${movie.Poster} class="movie-poster">
+                <div class="movie-description">
+                    <div class="movie-header">
+                        <h2 class="movie-title">${movie.Title}</h2>
+                        <div class="ratings">
+                            <img src="./images/ratings-icon.png" class="ratings-icon">
+                            <p class="ratings-text">${movie.Ratings[0].Value.slice(0,-3)}</p>
+                        </div>
+                    </div>
+                    <div class="movie-subheader">
+                        <p>${movie.Runtime}</p>
+                        <p>${movie.Genre}</p>
+                        <button class="watchlist-btn" data-imdbid=${movie.imdbID}>
+                            <span class="watchlist-icon" data-imdbid=${movie.imdbID}>+</span>
+                            <span data-imdbid=${movie.imdbID}>Watchlist</span>
+                        </button>
+                    </div>
+                    <p class="movie-plot">${movie.Plot}</p>
+                </div>
+            </section>
+            <hr>
+        </div>
+        `
+        
+    ).join()   
+})
+
+document.querySelector("main").addEventListener('click',function(e){
+    if(e.target.dataset.imdbid){
+        console.log(e.target.dataset.imdbid)
+        const selectedMovie = movieList.filter(movie => movie.imdbID === e.target.dataset.imdbid)
+        watchList.push(...selectedMovie)
+    }
+
 })
